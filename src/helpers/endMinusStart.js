a236f6fd497107ec111799c54882cbe42dee3934
Jeremy Bell <jeremy.bell91@gmail.com>
2021-04-14 13:16:08 +1000

    removed logging

--- src/helpers/endMinusStart.js
+++ src/helpers/endMinusStart.js
@@ -1,20 +1,10 @@
 import convertTimeToSeconds from './convertTimeToSeconds'
-import convertTimeFormat from './convertTimeFormat'
 
 const endMinusStart = (end, start) => {
-    console.log(end)
     let endSS = convertTimeToSeconds(end)
-    console.log(endSS)
-
-    console.log(start)
     let startSS = convertTimeToSeconds(start)
-    console.log(startSS)
-
     let difference = (endSS  - startSS).toFixed(3)
-    console.log('difference:', difference)
-
-    return convertTimeFormat(difference)
-
+    return difference
   }
 
 export default endMinusStart
