853c36ed8268d0fd252a8107fe24cef9a4364d95
Jeremy Bell <jeremy.bell91@gmail.com>
2021-04-30 15:35:06 +1000

    Cleaned up return

--- src/components/converter.js
+++ src/components/converter.js
@@ -10,6 +10,7 @@ import InputMask from "react-input-mask"
 import QualitySelection from '../components/qualityButtons'
 import ResetButton from './ResetButton'
 import VideoCard from './VideoCard'
+import Loading from './Loading'
 
 import AppContext from '../context/app-context'
 
@@ -123,133 +124,124 @@ const Converter = () => {
   }
 
 
-  return ready ? (
+  return !ready ? (
+        // Stage 0, display while loading ffmpeg 
+        <Loading />
+  ) : (
+    
+    
     <div className="App md:w-screen md:h-screen lg:bg-gray-900 bg-green-600 grid grid-cols-1 place-items-center">
 
-      {/* Initial Stage after loading */}
-      { !inputVideo &&
-        <DropArea />
-      }
+    {/* Stage 1: Show the drag and drop file area */}
+    { !inputVideo &&
+      <DropArea />
+    }
 
+    {/* Stage 2: Show the selected video and options for trimming and quality */}
+    {  (inputVideo && !converting && !outputVideo) &&
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 place-items-center gap-y-12">
+        <div class="max-w-3xl mx-auto">
+          <div className="bg-green-600 overflow-hidden rounded-lg max-w-xl h-full grid grid-cols-1 place-items-around gap-y-12">
+            <div className="px-4 py-5 sm:p-6 ">
 
-      {/* Converting */}
+              {(inputVideo && !outputVideo) &&
+                  <VideoCard video={"input"} />
+              }
+              {(!outputVideo) &&
+                <>
+                  <fieldset>
+                    <div className="flex justify-around w-full my-8 gap-4">
+                      <div className="flex-grow">
+                        <label htmlFor="start" className="sr-only">Start</label>
+                        <div className="mt-1 relative rounded-md shadow-sm">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <span className="text-gray-500 hidden md:inline">
+                              Start
+                  </span>
+                          </div>
+                          <InputMask formatChars={{
+                            '9': '[0-9]',
+                            '5': '[0-5]',
+                          }}
+                            mask="99:59:59.999" maskPlaceholder={'^'} defaultValue={startTrim} onBlur={(e) => handleChange(e, 'start')}>
+                            {(inputProps) => (
+                              <input
+                                {...inputProps}
+                                type="text"
+                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-center"
+                                name="start"
+                              />
+                            )}
+                          </InputMask>
+                        </div>
+                      </div>
 
-      { converting &&
-          <p className="text-2xl font-normal text-center text-green-600">Converting... It could take a while, please be patient <br></br><span className="text-2xl">If you can hear your computer fans it's probably working</span></p>
-      }
-
-      {/* View output */}
-      {outputVideo &&
-        <>
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 place-items-center gap-y-12">
-            <div class="max-w-3xl mx-auto">
-              <div className="bg-transparent overflow-hidden rounded-lg max-w-xl h-full grid grid-cols-1 place-items-around gap-y-12">
-                <div className="px-4 py-5 sm:p-6 ">
-                  <VideoCard video={"output"} />
+                      <div className="flex-grow">
+                        <label htmlFor="end" className="sr-only">End</label>
+                        <div className="mt-1 relative rounded-md shadow-sm">
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <span className="text-gray-500 hidden md:inline">
+                              End
+                            </span>
+                          </div>
+                          <InputMask formatChars={{
+                            '9': '[0-9]',
+                            '5': '[0-5]',
+                          }}
+                            mask="99:59:59.999" maskPlaceholder={0} defaultValue={endTrim} onBlur={(e) => handleChange(e, 'end')}>
+                            {(inputProps) => (
+                              <input
+                                {...inputProps}
+                                type="text"
+                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md w-24 text-center"
+                              />
+                            )}
+                          </InputMask>
+                        </div>
+                      </div>
+                    </div>
+                              <QualitySelection />
+                  </fieldset>
                   <div className="flex items-center justify-around gap-4 w-full mt-8">
-                    <a href={outputVideo.file} download={outputVideo.metadata.FileName} className="inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Download</a>
+                    <button type="button" className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={handleConvert}>Convert</button>
                     <ResetButton />
                   </div>
+                </>
+              }
+            </div>
+          </div>
+
+        </div>
+      </div>
+    }
+
+    
+    {/* Stage 3: Show message that the video is being converted */}
+    { converting &&
+        <p className="text-2xl font-normal text-center text-green-600">Converting... It could take a while, please be patient <br></br><span className="text-2xl">If you can hear your computer fans it's probably working</span></p>
+    }
+
+    {/* Stage 4: Show the converted output and provide option to save */}
+    {outputVideo &&
+      <>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 place-items-center gap-y-12">
+          <div class="max-w-3xl mx-auto">
+            <div className="bg-transparent overflow-hidden rounded-lg max-w-xl h-full grid grid-cols-1 place-items-around gap-y-12">
+              <div className="px-4 py-5 sm:p-6 ">
+                <VideoCard video={"output"} />
+                <div className="flex items-center justify-around gap-4 w-full mt-8">
+                  <a href={outputVideo.file} download={outputVideo.metadata.FileName} className="inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Save</a>
+                  <ResetButton />
                 </div>
               </div>
             </div>
           </div>
-
-        </>
-      }
-
-
-      {/* Trim and select quality */}
-
-
-      {  (inputVideo && !converting && !outputVideo) &&
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 place-items-center gap-y-12">
-          <div class="max-w-3xl mx-auto">
-            <div className="bg-green-600 overflow-hidden rounded-lg max-w-xl h-full grid grid-cols-1 place-items-around gap-y-12">
-              <div className="px-4 py-5 sm:p-6 ">
-
-                {(inputVideo && !outputVideo) &&
-                    <VideoCard video={"input"} />
-                }
-                {(!outputVideo) &&
-                  <>
-                    <fieldset>
-                      <div className="flex justify-around w-full my-8 gap-4">
-                        <div className="flex-grow">
-                          <label htmlFor="start" className="sr-only">Start</label>
-                          <div className="mt-1 relative rounded-md shadow-sm">
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                              <span className="text-gray-500 hidden md:inline">
-                                Start
-      </span>
-                            </div>
-                            <InputMask formatChars={{
-                              '9': '[0-9]',
-                              '5': '[0-5]',
-                            }}
-                              mask="99:59:59.999" maskPlaceholder={'^'} defaultValue={startTrim} onBlur={(e) => handleChange(e, 'start')}>
-                              {(inputProps) => (
-                                <input
-                                  {...inputProps}
-                                  type="text"
-                                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-center"
-                                  name="start"
-                                />
-                              )}
-                            </InputMask>
-                          </div>
-                        </div>
-
-                        <div className="flex-grow">
-                          <label htmlFor="end" className="sr-only">End</label>
-                          <div className="mt-1 relative rounded-md shadow-sm">
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                              <span className="text-gray-500 hidden md:inline">
-                                End
-      </span>
-                            </div>
-                            <InputMask formatChars={{
-                              '9': '[0-9]',
-                              '5': '[0-5]',
-                            }}
-                              mask="99:59:59.999" maskPlaceholder={0} defaultValue={endTrim} onBlur={(e) => handleChange(e, 'end')}>
-                              {(inputProps) => (
-                                <input
-                                  {...inputProps}
-                                  type="text"
-                                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md w-24 text-center"
-                                />
-                              )}
-                            </InputMask>
-                          </div>
-                        </div>
-                      </div>
-                                <QualitySelection />
-                    </fieldset>
-                    <div className="flex items-center justify-around gap-4 w-full mt-8">
-                      <button type="button" className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={handleConvert}>Convert</button>
-                      <ResetButton />
-                    </div>
-                  </>
-                }
-              </div>
-            </div>
-
-          </div>
         </div>
-      }
-    </div>
 
-  )
-    :
-    (
-      <div className="App md:w-screen md:h-screen lg:bg-gray-900 bg-green-600 grid grid-cols-1 place-items-center">
-        <div className="w-screen border-4 border-dashed h-screen bg-gray-900 grid grid-cols-1 place-items-center" >
-          <p className="text-2xl md:text-4xl lg:text-6xl xl:text-8xl font-bold text-center text-green-600">Loading</p>
-        </div>
-      </div>
+      </>
+    }
+  </div>
     )
-
 }
 
 export default Converter
