6d6d95ae9013b25e105d201f450a1d83a0982e02
Jeremy Bell <jeremy.bell91@gmail.com>
2021-05-03 15:25:51 +1000

    Removed conditional rendering from container. Instead using props.children.

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,18 +1,8 @@
-import ConvertButton from './ConvertButton'
-import ResetButton from './ResetButton'
-import SaveVideoLink from './SaveVideoLink'
+const ButtonContainer = () => {
 
-const ButtonContainer = ({ stage, ffmpeg }) => {
-
-    return stage === "editing" ? (
+    return (
         <div className="flex items-center justify-around gap-4 w-full mt-8">
-            <ConvertButton ffmpeg={ffmpeg} />
-            <ResetButton />
-        </div>
-    ) : (
-        <div className="flex items-center justify-around gap-4 w-full mt-8">
-            <SaveVideoLink />
-            <ResetButton />
+            {props.children}
         </div>
     )
 }
