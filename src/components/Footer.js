64469915d418fbff0f3a7f5bec5ae7fbf0030c06
Jeremy Bell <jeremy.bell91@gmail.com>
2021-05-04 13:05:00 +1000

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div className="mt-4">
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-700" />
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 bg-gray-700 text-white">Created by Jeremy Bell</span>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Footer
