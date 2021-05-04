a43460da7c7da49ae0ae27735f3800fdef86ff65
Jeremy Bell <jeremy.bell91@gmail.com>
2021-05-05 09:27:18 +1000

    Add hyperlink to my github profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="w-full border-t border-gray-700" />
           </div>
           <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-gray-700 text-white">Created by Jeremy Bell</span>
+            <span className="px-2 bg-gray-700 text-white"><a href="https://github.com/Jezzabell91/" class="py-2 px-4 flex justify-center items-center text-white hover:text-indigo-200  transition-colors duration-200 ">Created by Jeremy Bell</a></span>
           </div>
         </div>
       </div>
