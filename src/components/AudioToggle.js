475ac6d7d22bf4b6d112663e4f6aa566052bb3fa
Jeremy Bell <jeremy.bell91@gmail.com>
2021-05-16 21:00:26 +1000

    Styling

--- src/components/AudioToggle.js
+++ src/components/AudioToggle.js
@@ -18,14 +18,14 @@ const { removeAudio, setRemoveAudio} = useContext(AppContext)
       onChange={setRemoveAudio}
       className={classNames(
         removeAudio ? 'bg-indigo-600' : 'bg-gray-200',
-        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+        'relative ml-4 inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
       )}
     >
       <span className="sr-only">Use setting</span>
       <span
         className={classNames(
           removeAudio ? 'translate-x-5' : 'translate-x-0',
-          'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+          'pl-4 pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
         )}
       >
         <span
